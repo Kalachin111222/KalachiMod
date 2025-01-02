a94496862851b6001ad9acaafc19add2961b3a61
dsfAAEGAF <63252575+dsfAAEGAF@users.noreply.github.com>
2025-01-02 15:26:52 -0500

    02-01-2025

--- src/main/java/net/kalachi/tutorialmod/TutorialMod.java
+++ src/main/java/net/kalachi/tutorialmod/TutorialMod.java
@@ -2,6 +2,12 @@ package net.kalachi.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.kalachi.tutorialmod.block.ModBlocks;
+import net.kalachi.tutorialmod.item.ModItemGroups;
+import net.kalachi.tutorialmod.item.ModItems;
+import net.minecraft.client.model.ModelUtil;
+import net.minecraft.client.util.ModelIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +17,10 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 
+		FuelRegistry.INSTANCE.add(ModItems.STARLIGHT_ASHES,600);
 	}
 }
